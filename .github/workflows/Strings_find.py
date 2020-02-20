ae67e45f229f8d20ae836a0ffdb5478166a12f74
Nuria Sanz García <nuria.hilligehekken@gmail.com>
2020-02-20 17:40:02 +0100

    Add files via upload

---
+++ .github/workflows/Strings_find.py
@@ -0,0 +1,7 @@
+#STRING METHOD FIND	
+"""takes a string as an argument and searching the string it was run on for that string. It then returns the first index value where that string is located."""
+god_wills_it_line_one = "The very earth will disown you"
+
+disown_placement = god_wills_it_line_one.find("disown")
+print(disown_placement)
+"It will return 20"
