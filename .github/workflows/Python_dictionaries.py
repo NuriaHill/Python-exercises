ce89c9da9184822175b4447f947b0a3934ce1205
Nuria Sanz García <nuria.hilligehekken@gmail.com>
2020-03-03 17:39:16 +0100

    Add files via upload

---
+++ .github/workflows/Python_dictionaries.py
@@ -0,0 +1,10 @@
+#TEMPERATURE SENSOR DICTIONARY
+sensors =  {"living room": 21, "kitchen": 23, "bedroom": 20, "pantry": 22}
+print(sensors)
+#returns {'living room': 21, 'kitchen': 23, 'bedroom': 20, 'pantry': 22}
+
+#TRANSLATIONS DICTIONARY English-sindarin
+translations ={"mountain": "orod", "bread": "bass", "friend": "mellon", "horse": "roch"}
+
+#FAMILY DICTIONARY 
+children = {"von Trapp": ["Johannes", "Rosmarie", "Eleonore"], "Corleone": ["Sonny", "Fredo", "Michael"]}
