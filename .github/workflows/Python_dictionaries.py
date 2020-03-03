49674966c35e9f07ee06172f8ba8d7839ee9acdb
Nuria Sanz García <nuria.hilligehekken@gmail.com>
2020-03-03 17:48:35 +0100

    Add files via upload
    Added a new empty dictionary and additions

--- .github/workflows/Python_dictionaries.py
+++ .github/workflows/Python_dictionaries.py
@@ -8,3 +8,11 @@ translations ={"mountain": "orod", "bread": "bass", "friend": "mellon", "horse":
 
 #FAMILY DICTIONARY 
 children = {"von Trapp": ["Johannes", "Rosmarie", "Eleonore"], "Corleone": ["Sonny", "Fredo", "Michael"]}
+
+#ANIMALS IN ZOO WITH ADDITION DICTIONARY
+animals_in_zoo = {}
+animals_in_zoo["zebras"] = 8
+animals_in_zoo["monkeys"] = 12
+animals_in_zoo["dinosaurs"] = 0
+print(animals_in_zoo)
+#returns {'zebras': 8, 'monkeys': 12, 'dinosaurs': 0}
