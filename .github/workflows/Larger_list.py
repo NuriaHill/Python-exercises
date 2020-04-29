e7514c30f610da3ea3c721843e89c0549bf6fd1e
Nuria Sanz García <nuria.hilligehekken@gmail.com>
2020-04-29 12:01:26 +0200

    Delete Larger_list.py

--- .github/workflows/Larger_list.py
+++
@@ -1,8 +0,0 @@
-#this funtion return the last element of the list that contain more elements. 
-def larger_list(lst1,lst2):
-  if len(lst1)>=len(lst2):
-    return lst1[-1]
-  else: 
-    return lst2[-1]
-#testing the function:
-print(larger_list([4, 10, 2, 5], [-10, 2, 5, 10,6]))
