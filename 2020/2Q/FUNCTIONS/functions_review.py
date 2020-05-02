956104e2aefd61842aa98ae06e16c180be8e63a5
Nuria Sanz García <nuria.hilligehekken@gmail.com>
2020-05-02 22:38:48 +0200

    Add files via upload

---
+++ 2020/2Q/FUNCTIONS/functions_review.py
@@ -0,0 +1,9 @@
+def repeat_stuff(stuff, num_repeats=10):
+  return stuff * num_repeats
+
+repeat_stuff("Row ", 3)
+
+lyrics = repeat_stuff("Row ", 3) + "Your Boat. "
+song = repeat_stuff(lyrics)
+
+print(song)
