a3a1e2cf73c2af40b8dc4c0887de7907fc03533c
Nuria Sanz García <nuria.hilligehekken@gmail.com>
2020-05-09 13:21:20 +0200

    Add files via upload
    Added elif example

--- 2020/2Q/CONTROL FLOW/Boolean_expressions.py
+++ 2020/2Q/CONTROL FLOW/Boolean_expressions.py
@@ -64,3 +64,39 @@ def graduation_reqs(gpa, credits):
     return "Your GPA is not high enough to graduate."
   if not (gpa >= 2.0) and not (credits >= 120):
     return "You do not meet either requirement to graduate!"
+	
+#ELSE STATEMENTS
+
+def graduation_reqs(gpa, credits):
+  if (gpa >= 2.0) and (credits >= 120):
+    return "You meet the requirements to graduate!"
+  if (gpa >= 2.0) and not (credits >= 120):
+    return "You do not have enough credits to graduate."
+  if not (gpa >= 2.0) and (credits >= 120):
+    return "Your GPA is not high enough to graduate."
+  else : 
+    return "You do not meet the GPA or the credit requirement for graduation."
+
+#ELIF STATEMENTS
+def thank_you(donation):
+  if donation >= 1000:
+    print("Thank you for your donation! You have achieved platinum donation status!")
+  elif donation >= 500: 
+    print("Thank you for your donation! You have achieved gold donation status!")
+  elif donation >= 100:
+    print("Thank you for your donation! You have achieved silver donation status!")
+  else:
+    print("Thank you for your donation! You have achieved bronze donation status!")
+	
+def grade_converter (gpa):
+  if gpa >= 4.0:
+    grade = "A"
+  elif gpa >= 3.0:
+    grade = "B"
+  elif gpa >= 2.0:
+    grade = "C"
+  elif gpa >= 1.0:
+    grade = "D"
+  elif gpa >= 0.0:
+    grade = "F"
+  return grade
