0feecf1067942f20c2aa5cec893756bd9c97f635
Nuria Sanz García <nuria.hilligehekken@gmail.com>
2020-06-11 22:05:02 +0200

    Add files via upload

---
+++ 2020/2Q/LISTS/List_operations.py
@@ -0,0 +1,29 @@
+#LIST LENGTH
+list1 = range(2, 20, 2)
+
+list1_len = len(list1)
+print(list1_len) #9
+
+list1 = range(2, 20, 3)
+
+list1_len = len(list1)
+print(list1_len) #6
+
+
+#SELECTING LIST ELEMENTS
+
+employees = ['Michael', 'Dwight', 'Jim', 'Pam', 'Ryan', 'Andy', 'Robert']
+
+index4 = employees[4]
+print(index4)#Ryan
+
+shopping_list = ['eggs', 'butter', 'milk', 'cucumbers', 'juice', 'cereal']
+
+print(len(shopping_list))
+
+last_element = shopping_list[-1]
+
+element5 = shopping_list[5]
+
+print(last_element)#cereal
+print(element5)#cereal
